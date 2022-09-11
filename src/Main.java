8a6e2733d52f9de665672a716f62279921f639ba
YUSUF KUSCU <kyusuf40ky@gmail.com>
2022-09-11 21:12:01 +0300

    Siralandi

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("1. Sayı : ");
+        a = scan.nextInt();
+        System.out.print("2. Sayı : ");
+        b = scan.nextInt();
+        System.out.print("3. Sayı : ");
+        c = scan.nextInt();
+
+        if((a < b) && (a < c)){
+            if (b < c){
+                System.out.println("a < b < c");
+            }else{
+                System.out.println("a < c < b");
+            }
+        }else if((b < a) && (b < c)){
+            if (a < c){
+                System.out.println("b < a < c");
+            }else {
+                System.out.println("b < c < a");
+            }
+        }else {
+            if (b < a){
+                System.out.println("c < b < a");
+            }else {
+                System.out.println("c < a < b");
+            }
+        }
+
+    }
+}
